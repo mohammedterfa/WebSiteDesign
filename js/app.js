6523ffbbd262a1f6fa89f48c456c605308d8b00b
mohammedterfa <mohammedterfa@gmail.com>
2022-07-12 17:21:46 +0200

    script added

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const header = document.querySelector("header");
+/*----------------- Sticky Navbar --------------------*/
+
+function stickyNavbar(){
+    header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+
+window.addEventListener("scroll", stickyNavbar);
