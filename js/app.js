e807a1121b2bb272427a816c9d6d623df19c97d3
mohammedterfa <mohammedterfa@gmail.com>
2022-09-22 12:31:20 +0200

    about section some content added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const header = document.querySelector("header");
 /*----------------- Sticky Navbar --------------------*/
 
+
 function stickyNavbar(){
     header.classList.toggle("scrolled", window.pageYOffset > 0);
 }
@@ -9,3 +10,13 @@ stickyNavbar();
 
 
 window.addEventListener("scroll", stickyNavbar);
+
+/*----------------- Reveal Animation  --------------------*/
+
+let sr = ScrollReveal({
+    duration:2500,
+    distance:"60px",
+});
+
+sr.reveal(".showcase-info",{delay: 600});
+sr.reveal(".showcase-image",{origin: "top", delay: 700});
