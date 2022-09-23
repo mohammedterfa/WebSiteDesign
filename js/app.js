21373d63016dd9948d739dcdf57d21043a3f6bbd
mohammedterfa <mohammedterfa@gmail.com>
2022-09-23 21:36:53 +0200

    testimonails section ended

--- js/app.js
+++ js/app.js
@@ -164,3 +164,18 @@ function changeImage(index){
     images[index].classList.add("showImage");
 }
 
+/*----------------- Modal Pop Up  Animation --------------------*/
+
+const swiper = new Swiper('.swiper', {
+  
+    loop: true,
+    speed:500,
+    autoplay:true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+  });
+
