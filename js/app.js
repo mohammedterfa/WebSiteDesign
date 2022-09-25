080b1a3e821b3f9295c3ebd5cee897b08415465b
mohammedterfa <mohammedterfa@gmail.com>
2022-09-25 18:12:45 +0200

    media 1000px is now responsive

--- js/app.js
+++ js/app.js
@@ -208,16 +208,23 @@ const swiper = new Swiper('.swiper', {
 
    /*----------------- change page theme --------------------*/
 
-function changeTheme(){
-    if(!document.body.classList.contains("dark")){
+let firstTheme = localStorage.getItem("dark");
+
+changeTheme(+firstTheme);
+
+function changeTheme(isDark){
+    if(isDark){
         document.body.classList.add("dark");
         toggle_btn.classList.replace("uil-moon", "uil-sun");
+        localStorage.setItem("dark", 1);
     }else{
         document.body.classList.remove("dark");
         toggle_btn.classList.replace("uil-sun", "uil-moon");
+        localStorage.setItem("dark", 0);
+
     }
 }
 
 toggle_btn.addEventListener("click", () => {
-    changeTheme();
+    changeTheme(!document.body.classList.contains("dark"));
 });
